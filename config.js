53430b9ce568c3fca31308e9df2c138c7062d6b6
ksr2006 <147728117+ksr2006@users.noreply.github.com>
2023-10-15 16:59:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94701843725', 'me', true],
+  ['94701843725', 'KSR', true], 
   [''] 
 ] //Number of owners
 
